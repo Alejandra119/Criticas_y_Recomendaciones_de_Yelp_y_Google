0e73bd325acbbc4b1fc753f89d41d61696ad48a4
ozzy <flavioboviovt@gmail.com>
2023-10-17 14:35:45 -0300

    streamlit AS

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -3,11 +3,25 @@
 import streamlit as st
 import pandas as pd
 import gcsfs
-
-# Crea un objeto GcsFileSystem para acceder al bucket de GCP
-fs = gcsfs.GCSFileSystem(project='steakhouses')
+from functions import *
 
 
-file = fs.open('bucket-steakhouses/score (1).csv')
-df = pd.read_csv(file)
-st.write(df)
+# # Crea un objeto GcsFileSystem para acceder al bucket de GCP
+# fs = gcsfs.GCSFileSystem(project='steakhouses2')
+# file = fs.open('bucket-steakhouses2/score (1).csv')
+# df = pd.read_csv(file)
+# st.write(df)
+
+def mostrar_as(negocio):
+    id = negocios.loc[negocios['name']==negocio]['business_id'].iloc[0]
+    reviews = API_reviews(id)
+    as_reviews =  sentimientos_Yelp (reviews)
+    st.write(as_reviews)
+
+
+
+negocios = negocios()
+negocio_seleccion = st.sidebar.selectbox('**Negocio**', options=negocios['name'])
+boton_consultar = st.sidebar.button('Consultar', on_click=mostrar_as(negocio_seleccion))
+
+
