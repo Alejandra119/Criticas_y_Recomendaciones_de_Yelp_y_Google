736db480e3c34d8a5f11fa8c0d4af563b122c550
smlopez30 <smlopez@gmail.com>
2023-10-17 15:19:15 -0300

    Google

--- ETL/DAG_ETL_GOOGLE_transform.py
+++ ETL/DAG_ETL_GOOGLE_transform.py
@@ -6,6 +6,15 @@ from datetime import timedelta
 import pandas as pd
 import re
 import pyarrow.parquet as pq
+import os
+from google.cloud import storage
+import json
+
+import attr
+
+@attr.s
+class MyDataFrame(pd.DataFrame):
+    pass
 
 # Argumentos por defecto del DAG
 default_args = {
@@ -26,44 +35,59 @@ dag = DAG(
 )
 
 # Tarea para leer todos los archivos JSON
-def read_all_json_files(input_path):
-    """Lee todos los archivos JSON en un directorio.
+def process_all_json_files(input_path):
+    """Lee todos los archivos JSON en un bucket de GCS.
 
     Args:
-        input_path (str): Ruta al directorio que contiene los archivos JSON.
+        input_path (str): Ruta al bucket y directorio que contiene los archivos JSON.
 
     Returns:
         list[dict]: Una lista de diccionarios JSON.
     """
+    # Dividir la ruta de GCS en bucket y prefix
+    bucket_name, prefix = input_path.replace("gs://", "").split("/", 1)
 
-    json_files = []
-    for filename in os.listdir(input_path):
-        if filename.endswith('.json'):
-            json_files.append(os.path.join(input_path, filename))
+    # Crear un cliente de GCS
+    client = storage.Client()
+    bucket = client.get_bucket(bucket_name)
+
+    # Listar los blobs en el bucket usando el prefijo
+    blobs = bucket.list_blobs(prefix=prefix)
 
     json_data = []
-    for json_file in json_files:
-        with open(json_file, 'r') as f:
-            json_data.append(json.load(f))
+    for blob in blobs:
+        # Leer el blob como una cadena y cargar el JSON
+        json_str = blob.download_as_text()
+        try:
+            json_data.append(json.loads(json_str))
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON from blob {blob.name}. Blob is empty or not valid JSON.")
+            # O puedes usar logging en lugar de print para registrar el error
+            # logging.error(f"Error decoding JSON from blob {blob.name}. Blob is empty or not valid JSON.")
 
     return json_data
-
 # Tarea para juntar todos los datos JSON en un solo DataFrame
-def merge_json_data(json_data):
+def merge_json_data(input_path, output_path):
     """Junta todos los datos JSON en un solo DataFrame.
 
     Args:
-        json_data (list[dict]): Una lista de diccionarios JSON.
+        input_path (str): Ruta al archivo que contiene los datos JSON.
+        output_path (str): Ruta al archivo Parquet de salida.
 
     Returns:
-        pandas.DataFrame: Un DataFrame que contiene todos los datos JSON.
+        str: Ruta al archivo Parquet de salida.
     """
+    # Leer los datos JSON desde input_path
+    with open(input_path, 'r') as f:
+        json_data = json.load(f)
 
-    df = pd.DataFrame(json_data[0])
-    for i in range(1, len(json_data)):
-        df = pd.concat([df, pd.DataFrame(json_data[i])])
+    # ... [Resto de la lógica de la función]
 
-    return df
+    # Guardar el DataFrame como un archivo Parquet en output_path
+    df.to_parquet(output_path, index=False)
+
+    return output_path
+
 
 # Tarea para procesar los datos JSON
 def process_json_data(df):
@@ -91,33 +115,28 @@ def write_dataframe_to_parquet(df, output_path):
 
     df.to_parquet(output_path, index=False)
 
-def process_all_json_files(input_path):
-    """Lee todos los archivos JSON en un directorio.
-
-    Args:
-        input_path (str): Ruta al directorio que contiene los archivos JSON.
+def check_files_exist():
+    """Comprueba si los archivos JSON están disponibles.
 
     Returns:
-        list[dict]: Una lista de diccionarios JSON.
+        bool: True si los archivos JSON están disponibles, False si no.
     """
+    # Obtener la lista de archivos JSON
+    blob_names = ["gs://bucket-steakhouses2/crudo/google/reviews-estados/" + blob.name for blob in bucket.list_blobs(prefix="")]
 
-    json_files = []
-    for filename in os.listdir(input_path):
-        if filename.endswith('.json'):
-            json_files.append(os.path.join(input_path, filename))
+    # Verificar si los archivos JSON existen
+    for blob_name in blob_names:
+        if not bucket.get_blob(blob_name).exists():
+            return False
 
-    json_data = []
-    for json_file in json_files:
-        with open(json_file, 'r') as f:
-            json_data.append(json.load(f))
-
-    return json_data
+    return True
 
 # Tarea para procesar todos los archivos JSON
 t1 = PythonOperator(
     task_id='process_all_json_files',
     python_callable=process_all_json_files,
-    op_args=['gs://bucket-pghenry-dpt2/google_sitios/'],
+    op_args=['gs://bucket-steakhouses2/crudo/google'],
+    op_kwargs={'output_path': 'gs://bucket-steakhouses2/crudo/results.json'},  # Añadir un argumento para la ruta de salida
     dag=dag,
 )
 
@@ -125,7 +144,7 @@ t1 = PythonOperator(
 t2 = PythonOperator(
     task_id='merge_json_data',
     python_callable=merge_json_data,
-    op_args=[t1.output],
+    op_args=['gs://bucket-steakhouses2/crudo/results.json', 'gs://bucket-steakhouses2/filtrado/metadata_sitios.parquet'],  # Usar rutas de archivos en lugar de objetos
     dag=dag,
 )
 
@@ -141,23 +160,32 @@ t3 = PythonOperator(
 t4 = PythonOperator(
     task_id='write_dataframe_to_parquet',
     python_callable=write_dataframe_to_parquet,
-    op_args=[t3.output, 'gs://bucket-pghenry-dpt2/google/metadata_sitios.parquet'],
+    op_args=[t3.output, 'gs://bucket-steakhouses2/filtrado/metadata_sitios.parquet'],
     dag=dag,
 )
+
+
 
 # Dependencias entre tareas
 t1 >> t2 >> t3 >> t4
 
-# Tarea para procesar todos los archivos JSON en review_estados
 t5 = PythonOperator(
     task_id='process_all_json_files_2',
     python_callable=process_all_json_files,
-    op_args=['gs://bucket-pghenry-dpt2/crudo/google_estados/review_Pennsylvania/'],
+    op_args=['gs://bucket-steakhouses2/crudo/google/reviews-estados'],
+    dag=dag,
+)
+
+# Tarea para procesar todos los archivos JSON en review_estados
+t6 = PythonOperator(
+    task_id='process_all_json_files_2',
+    python_callable=process_all_json_files,
+    op_args=['gs://bucket-steakhouses2/crudo/google/reviews-estados'],
     dag=dag,
 )
 
 # Tarea para juntar todos los datos JSON en un solo DataFrame para review_estados
-t6 = PythonOperator(
+t7 = PythonOperator(
     task_id='merge_json_data_2',
     python_callable=merge_json_data,
     op_args=[t5.output],
@@ -165,7 +193,7 @@ t6 = PythonOperator(
 )
 
 # Tarea para procesar los datos JSON para la nueva carpeta
-t7 = PythonOperator(
+t8 = PythonOperator(
     task_id='process_json_data_2',
     python_callable=process_json_data,
     op_args=[t6.output],
@@ -173,12 +201,12 @@ t7 = PythonOperator(
 )
 
 # Tarea para escribir el DataFrame a un nuevo archivo Parquet
-t8 = PythonOperator(
+t9 = PythonOperator(
     task_id='write_dataframe_to_parquet_2',
     python_callable=write_dataframe_to_parquet,
-    op_args=[t7.output, 'gs://bucket-pghenry-dpt2/review_estados.parquet'],
+    op_args=[t7.output, 'gs://bucket-steakhouses2/filtrado/review_estados.parquet'],
     dag=dag,
 )
 
 # Dependencias entre tareas para la nueva carpeta
-t5 >> t6 >> t7 >> t8
+t5 >> t6 >> t7 >> t8 >> t9
