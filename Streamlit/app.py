e253c11a46a4a9f480069f6909371c3c449159fd
Aleli <asalastal@unsa.edu.pe>
2023-10-17 20:53:12 -0500

    add_code

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -5,6 +5,11 @@ import pandas as pd
 import gcsfs
 from functions import *
 
+st.set_page_config(page_icon="📊", page_title="Clasificación de reseñas", layout="wide")
+st.image("https://static.wixstatic.com/media/0f55e2_c8cb97b88d3d4e728390c20e7295c26e~mv2.png/v1/fill/w_148,h_78,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/barritas%20logo.png", width=200)
+st.title("Clasificación de reseñas a partir de la API de Yelp")
+c29, c30, c31 = st.columns([1, 6, 1]) # 3 columnas: 10%, 60%, 10%
+
 
 # # Crea un objeto GcsFileSystem para acceder al bucket de GCP
 # fs = gcsfs.GCSFileSystem(project='steakhouses2')
